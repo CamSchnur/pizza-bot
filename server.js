4909929bbee6c69f6ad5ec8bdd6a00e595edb173
CamSchnur <42192713+CamSchnur@users.noreply.github.com>
2019-09-19 20:48:20 -0700

    the bot can now send a tweet

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ var fs = require('fs'),
 path = require('path'),
 Twit = require('twit'),
 config = require(path.join(__dirname, 'config.js'))
+
+
+var T = new Twit(config);
+
+T.post('statuses/update', {status: 'Test tweet!'}, function(err, data, response){ console.log (data)});
