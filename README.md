2ef43cca96a568c9cae7ef3179c1d639759ff5f5
CamSchnur <42192713+CamSchnur@users.noreply.github.com>
2019-09-19 22:00:05 -0700

    add cheerio library for easier parsing HTML result from twemoji

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pizza-bot
- it's a pizza bot
+ 
+
+it's a pizza bot
+
+
+uses twemoji https://twemoji.twitter.com/ which uses the MIT license 
+uses cheerio https://github.com/cheeriojs/cheerio which uses the MIT license
--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 var fs = require('fs'),
 path = require('path'),
 Twit = require('twit'),
+twemoji = require('twemoji'),
+cheerio = require('cheerio'),
 config = require(path.join(__dirname, 'config.js'))
 
 
-var T = new Twit(config);
+var imagePath = get_random_topping();
 
-T.post('statuses/update', {status: 'Test tweet!'}, function(err, data, response){ console.log (data)});
+var T = new Twit(config);
+T.post('statuses/update', {status: imagePath}, function(err, data, response){ console.log (data)});
+
+
+function combine_toppings_and_crust()
+{
+
+}
+
+function get_random_topping()
+{
+    console.log('getting topping');
+
+    var div = twemoji.parse('\ud83c\udf55');
+    var parsed = cheerio.load(div);
+
+    //console.log(parsed);
+    var src = parsed('img').attr('src');
+
+    console.log(src);
+    
+    return src;  
+
+}
+
+function generate_crust()
+{
+
+}
